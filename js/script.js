b4e8db7dd32195421465cd37b2198706101bc3e8
apkoha <russiapatriot2@yandex.ru>
2023-12-07 17:59:53 +0300

    день 2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiperThumb = new Swiper(".gift__swiper_thumb", {
+  spaceBetween: 16,
+  slidesPerView: 6,
+  freeMode: true,
+});
+
+const swiperMain = new Swiper(".gift__swiper_card", {
+  spaceBetween: 16,
+  thumbs: {
+    swiper: swiperThumb,
+  },
+});
